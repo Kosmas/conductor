120a9940566bb7b4417b22bf9354cdbe54d6411f
Kosmas Chatzimichalis <kosmas@mach7x.com>
2013-09-09 13:34:07 +0200

    added missing description field

--- db/migrate/20130906093312_create_projects.rb
+++ db/migrate/20130906093312_create_projects.rb
@@ -2,6 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :name
+      t.string :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20130906093312) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
