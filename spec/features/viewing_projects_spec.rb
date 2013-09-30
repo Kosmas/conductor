683356774c4182d4a77d38c7c4ebbdc4f2879055
Kosmas Chatzimichalis <kosmas@mach7x.com>
2013-09-30 14:09:12 +0200

    Add the ability to view a list of all projects

---
+++ spec/features/viewing_projects_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature "Viewing projects" do
+  scenario "Listing all projects" do
+    project = FactoryGirl.create(:project, name: "Vim")
+    visit '/'
+    click_link "Vim"
+    expect(page.current_url).to eql(project_url(project))
+  end
+end
