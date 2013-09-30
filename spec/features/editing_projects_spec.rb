738869613f16cdb2275f2c61ad41b245d31cf377
Kosmas Chatzimichalis <kosmas@mach7x.com>
2013-09-30 22:36:52 +0200

    Added tests and functionality for updating a project.

---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature "Editing Projects" do
+  before do
+    FactoryGirl.create(:project, name: "Vim")
+
+    visit "/"
+    click_link "Vim"
+    click_link "Edit Project"
+  end
+
+  scenario "Updating a project" do
+    fill_in "Name", with: "Vim beta"
+    click_button "Update Project"
+
+    expect(page).to have_content("Project has been updated.")
+  end
+
+  scenario "Updating a project with invalid attributes is bad" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+
+    expect(page).to have_content("Project has not been updated.")
+  end
+end
