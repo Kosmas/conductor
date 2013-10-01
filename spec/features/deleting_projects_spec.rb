951deae1b0d136b97305052649d950eb82fd5f6c
Kosmas Chatzimichalis <kosmas@mach7x.com>
2013-10-01 21:18:02 +0200

    Implement delete functionality for projects

---
+++ spec/features/deleting_projects_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature "Deleting projects" do
+  scenario "Deleting a project" do
+    FactoryGirl.create(:project, name: "Vim")
+
+    visit "/"
+    click_link "Vim"
+    click_link "Delete Project"
+
+    expect(page).to have_content("Project has been destroyed.")
+
+    visit "/"
+
+    expect(page).to have_no_content("Vim")
+  end
+end
