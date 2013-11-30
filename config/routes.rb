5f13a2e55ca20d4a03b27104446e52064dfdc179
Kosmas Chatzimichalis <kosmas@mach7x.com>
2013-12-01 00:54:28 +0100

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -61,4 +61,7 @@ Conductor::Application.routes.draw do
   end
 
 	resources :users
+
+	get '/signin', to: 'sessions#new'
+	post '/signin', to: 'sessions#create'
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
 	factory :user do
+		name 'tester'
 		email 'test@test.com'
 		password 'password'
 		password_confirmation	'password'
