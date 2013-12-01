7f2464fa1c1be456e214f00515137b39db6afa56
Kosmas Chatzimichalis <kosmas@mach7x.com>
2013-12-01 20:26:23 +0100

    Changed hash syntax to > 1.9 version

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.where(:name => params[:signin][:name]).first
+		user = User.where(name: params[:signin][:name]).first
 
 		if user && user.authenticate(params[:signin][:password])
 	
