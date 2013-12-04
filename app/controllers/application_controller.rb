8a081efe56073474cb5268935bc799ec179e28cd
Kosmas Chatzimichalis <kosmas@mach7x.com>
2013-12-04 18:19:39 +0100

    Add admin namespaced users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,15 @@ class ApplicationController < ActionController::Base
 	end
 	helper_method :require_signin!
 
+	def authorize_admin!
+    require_signin!
+
+    unless current_user.admin?
+      flash[:alert] = 'You must be an admin to do that.'
+      redirect_to root_path
+    end
+  end
+
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -49,15 +49,6 @@ class ProjectsController < ApplicationController
 
   private
 
-	def authorize_admin!
-		require_signin!
-
-		unless current_user.admin?
-			flash[:alert] = 'You must be an admin to do that.'
-			redirect_to root_path
-		end
-	end
-
   def project_params
     params.require(:project).permit(:name, :description)
   end
