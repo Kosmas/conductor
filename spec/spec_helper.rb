f874c45f1ee2b6d1de584e04db5dd1db165b0a5e
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-06-30 20:17:13 +0200

    changed spec_helper to make tests pass after upgrading to rspec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -39,4 +38,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # After upgrading to rspec 3
+  config.infer_spec_type_from_file_location!
 end
