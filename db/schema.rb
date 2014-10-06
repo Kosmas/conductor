3356e6b361571c3340a80e17916770e9f84082bf
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-10-06 11:06:34 +0200

    updated schema with latest migrations

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140925142612) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "state_id"
   end
 
   add_index "comments", ["ticket_id"], name: "index_comments_on_ticket_id"
@@ -53,6 +54,12 @@ ActiveRecord::Schema.define(version: 20140925142612) do
     t.datetime "updated_at"
   end
 
+  create_table "states", force: true do |t|
+    t.string "name"
+    t.string "color"
+    t.string "background"
+  end
+
   create_table "tickets", force: true do |t|
     t.string   "title"
     t.text     "description"
@@ -60,6 +67,7 @@ ActiveRecord::Schema.define(version: 20140925142612) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "state_id"
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
