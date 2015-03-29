fb2ec71ca9965ed88c4240f9931d024c3efa204a
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-29 13:49:06 +0200

    modified for latest MEAP version

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-feature 'Signing up' do
-  scenario 'Succussful sign up' do
+feature 'Users can sign up' do
+  scenario 'when providing valid details' do
     visit '/'
 
     click_link 'Sign up'
-    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Email', with: 'test@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
     click_button 'Sign up'
