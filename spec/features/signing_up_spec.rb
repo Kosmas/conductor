8f75256c55ce131bd97f007b0fb25301cae67de8
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-29 13:51:45 +0200

    modified password field for future use to avoid ambigous match error

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -6,7 +6,7 @@ feature 'Users can sign up' do
 
     click_link 'Sign up'
     fill_in 'Email', with: 'test@example.com'
-    fill_in 'Password', with: 'password'
+    fill_in 'user_password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
     click_button 'Sign up'
 
