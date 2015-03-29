dd5bd4dc4d4fec9765672fd5691d3ac66d30c059
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-29 13:44:32 +0200

    update schema with current migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,75 +11,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925142612) do
+ActiveRecord::Schema.define(version: 20150329112800) do
 
-  create_table "assets", force: true do |t|
-    t.string   "asset"
-    t.integer  "ticket_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "content_type"
-  end
-
-  add_index "assets", ["ticket_id"], name: "index_assets_on_ticket_id"
-
-  create_table "comments", force: true do |t|
-    t.text     "text"
-    t.integer  "ticket_id"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "state_id"
-  end
-
-  add_index "comments", ["ticket_id"], name: "index_comments_on_ticket_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
-
-  create_table "permissions", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "thing_id"
-    t.string   "thing_type"
-    t.string   "action"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "permissions", ["thing_id", "thing_type"], name: "index_permissions_on_thing_id_and_thing_type"
-  add_index "permissions", ["user_id"], name: "index_permissions_on_user_id"
-
-  create_table "projects", force: true do |t|
+  create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  create_table "states", force: true do |t|
-    t.string "name"
-    t.string "color"
-    t.string "background"
-  end
-
-  create_table "tickets", force: true do |t|
+  create_table "tickets", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.integer  "project_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "user_id"
-    t.integer  "state_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
-  add_index "tickets", ["user_id"], name: "index_tickets_on_user_id"
 
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",           default: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
