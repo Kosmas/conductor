8fecd53b9721a1744335522baf3024aea569f243
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-03-29 13:43:48 +0200

    modified user model to use devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
-  has_secure_password
-
-  has_many :permissions
-
-  validates :email, presence: true
-
-  def to_s
-    "#{email} (#{admin? ? 'Admin' : 'User'})"
-  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
