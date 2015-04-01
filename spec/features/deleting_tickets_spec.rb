4a707a043c6b821f14bb6c65921586e423b036d5
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-01 15:39:15 +0200

    added user and updated spec for latest MEAP release

--- spec/features/deleting_tickets_spec.rb
+++ spec/features/deleting_tickets_spec.rb
@@ -2,23 +2,16 @@ require 'spec_helper'
 
 feature 'Deleting tickets' do
   let!(:project) { FactoryGirl.create(:project) }
-  let!(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user) }
   let!(:ticket) do
-    ticket = FactoryGirl.create(:ticket, project: project) 
-    ticket.update(user: user)
-    ticket
+    FactoryGirl.create(:ticket, project: project, author: user)
   end
 
   before do
-    define_permission!(user, 'view', project)
-    define_permission!(user, 'delete tickets', project)
-    sign_in_as!(user)
-    visit '/'
-    click_link project.name
-    click_link ticket.title
+    visit project_ticket_path(project, ticket)
   end
 
-  scenario 'Deleting a ticket' do
+  scenario 'deleting successfully' do
     click_link 'Delete Ticket'
 
     expect(page).to have_content('Ticket has been deleted.')
