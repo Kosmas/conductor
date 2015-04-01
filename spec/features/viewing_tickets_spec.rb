c276fe26836ca1c468747935821befedb270c55e
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-01 15:59:40 +0200

    added user and updated spec for latest MEAP release

--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -4,18 +4,23 @@ feature 'Viewing tickets' do
   before do
     user = FactoryGirl.create(:user)
     vim_2 = FactoryGirl.create(:project, name: 'Vim2')
-    ticket_1 = FactoryGirl.create(:ticket, project: vim_2, title: 'Make it shiny!', description: 'Gradients! Starbursts! Oh my!')
-    ticket_1.update(user: user)
-    define_permission!(user, 'view', vim_2)
+    FactoryGirl.create(:ticket,
+                       project: vim_2,
+                       title: 'Make it shiny!',
+                       description: 'Gradients! Starbursts! Oh my!',
+                       author: user)
+
     internet_explorer = FactoryGirl.create(:project, name: 'Internet Explorer')
-    ticket_2 = FactoryGirl.create(:ticket, project: internet_explorer, title: 'Standards compliance', description: "Isn't a joke.")
-    ticket_2.update(user: user)
-    define_permission!(user, 'view', internet_explorer)
-    sign_in_as!(user)
+    FactoryGirl.create(:ticket,
+                       project: internet_explorer,
+                       title: 'Standards compliance',
+                       description: "Isn't a joke.",
+                       author: user)
+
     visit '/'
   end
 
-  scenario 'Viewing tickets for a given project' do
+  scenario 'for a given project' do
     click_link 'Vim2'
 
     expect(page).to have_content('Make it shiny!')
