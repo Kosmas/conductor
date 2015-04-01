c5f6c0698746fb56a1a88c1ce36eb2bada279d70
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-01 15:11:06 +0200

    Migration for adding author id to tickets and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329112800) do
+ActiveRecord::Schema.define(version: 20150401130456) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150329112800) do
     t.integer  "project_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "author_id"
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
