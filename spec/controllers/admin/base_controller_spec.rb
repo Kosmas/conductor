e43f3e442e2d8df5f10a32dc232d344721c88fb7
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-04 17:25:12 +0200

    add spec for admin controller normal user redirection

---
+++ spec/controllers/admin/base_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe Admin::BaseController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+
+  before do
+    allow(controller).to receive(:current_user).and_return(user)
+  end
+
+  context 'standard users' do
+    it 'are not able to access the index action' do
+      get :index
+
+      expect(response).to redirect_to('/')
+      expect(flash[:alert]).to eql('You must be an admin to do that.')
+    end
+  end
+end
