56077df6125fbabfca81599297cfc3c057f9e705
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-05 21:12:10 +0200

    add admin user login

--- spec/features/admin/deleting_projects_spec.rb
+++ spec/features/admin/deleting_projects_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 feature 'Deleting projects' do
-#  before do
-#    sign_in_as!(FactoryGirl.create(:admin_user))
-#  end
+  before do
+    login_as(FactoryGirl.create(:user, :admin))
+  end
 
   scenario 'Deleting a project' do
     FactoryGirl.create(:project, name: 'Vim')
