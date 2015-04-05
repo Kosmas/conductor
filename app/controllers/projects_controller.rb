084a9be9de6318b3d18ec0a21ae7d90520ed503d
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-05 21:14:18 +0200

    moved out new, create and destroy methods to the admin project controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,30 +1,8 @@
 class ProjectsController < ApplicationController
-  before_action :authorize_admin!, except: [:index, :show]
-  before_action :require_signin!, only: [:index, :show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.for(current_user)
-  end
-
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      flash[:notice] = "Project has been created."
-      redirect_to @project
-    else
-      flash[:alert] = "Project has not been created."
-
-      render "new"
-    end
-  end
-
-  def show
+    @projects = Project.all
   end
 
   def edit
@@ -32,32 +10,26 @@ class ProjectsController < ApplicationController
 
   def update
     if @project.update(project_params)
-      flash[:notice] = "Project has been updated."
+      flash[:notice] = 'Project has been updated.'
       redirect_to @project
     else
-      flash[:alert] = "Project has not been updated."
-      render "edit"
+      flash.now[:alert] = 'Project has not been updated.'
+      render 'edit'
     end
-  end
-
-  def destroy
-    @project.destroy
-
-    flash[:notice] = "Project has been destroyed."
-
-    redirect_to projects_path
   end
 
   private
 
-  def project_params
-    params.require(:project).permit(:name, :description)
+  def set_project
+    @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = 'The project you were looking' +
+                      ' for could not be found.'
+
+      redirect_to projects_path
   end
 
-  def set_project
-    @project = Project.for(current_user).find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-    flash[:alert] = "The project you were looking for could not be found."
-    redirect_to projects_path
+  def project_params
+    params.require(:project).permit(:name, :description)
   end
 end
