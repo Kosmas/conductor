dd1e858207c59ca33fbf57fd8cc7a2ede025525b
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-06 16:44:15 +0200

    add admins_only method and update for current MEAP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,7 @@ module ApplicationHelper
     end
   end
 
-	def admins_only(&block)
-		block.call if current_user.try(:admin?)
-	end
-
-  def authorized?(permission, thing, &block)
-    block.call if can?(permission.to_sym, thing) || current_user.try(:admin?)
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
   end
 end
