686b8fd93648c520578b04ce679b8e33bedb5ff5
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-09 13:37:53 +0200

    add spec for admin users

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -1,21 +1,31 @@
 require 'spec_helper'
 
 feature 'An admin can create a new user' do
-	let!(:admin) { FactoryGirl.create(:user, :admin) }
+  let!(:admin) { FactoryGirl.create(:user, :admin) }
 
-	before do
-		login_as(admin)
-		visit '/'
-		click_link 'Admin'
-		click_link 'Users'
-		click_link 'New User'
-	end
+  before do
+    login_as(admin)
+    visit '/'
+    click_link 'Admin'
+    click_link 'Users'
+    click_link 'New User'
+  end
 
-	scenario 'with valid credentials' do
-		fill_in 'Email', with: 'newbie@example.com'
-		fill_in 'Password', with: 'password'
-		click_button 'Create User'
+  scenario 'with valid credentials' do
+    fill_in 'Email', with: 'newbie@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Create User'
 
-		expect(page).to have_content('User has been created.')
-	end
+    expect(page).to have_content('User has been created.')
+  end
+
+  scenario 'when the new user is an admin' do
+    fill_in 'Email', with: 'admin@example.com'
+    fill_in 'Password', with: 'password'
+    check 'Is an admin?'
+    click_button 'Create User'
+
+    expect(page).to have_content('User has been created.')
+    expect(page).to have_content('admin@example.com (Admin)')
+  end
 end
