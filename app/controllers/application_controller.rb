ef0ec0089184ef26233717355db8811dcb9a91b1
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-11 15:04:43 +0200

    update for current MEAP and add include Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,28 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-
-  def require_signin!
-    if current_user.nil?
-      flash[:error] = 'You need to sign in or sign up before continuing.'
-
-      redirect_to signin_url
-    end
-  end
-  helper_method :require_signin!
-
-  def authorize_admin!
-    require_signin!
-
-    unless current_user.admin?
-      flash[:alert] = 'You must be an admin to do that.'
-      redirect_to root_path
-    end
-  end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
+  include Pundit
 end
