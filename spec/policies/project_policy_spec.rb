68f9e3ffe4737e02e199d49627af96ad240686b9
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-13 13:39:48 +0200

    add tests for show? and remove unused specs

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -6,23 +6,38 @@ describe ProjectPolicy do
 
   subject { ProjectPolicy }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
   permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    let(:user) { FactoryGirl.create(:user) }
+    let(:project) { FactoryGirl.create(:project) }
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    it 'blocks anonymous users' do
+      expect(subject).not_to permit(nil, project)
+    end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'allows viewers of the project' do
+      assign_role!(user, :viewer, project)
+      expect(subject).to permit(user, project)
+    end
+
+    it 'allows editors of the project' do
+      assign_role!(user, :editor, project)
+      expect(subject).to permit(user, project)
+    end
+
+    it 'allows managers of the project' do
+      assign_role!(user, :manager, project)
+      expect(subject).to permit(user, project)
+    end
+
+    it 'allows administrators' do
+      admin = FactoryGirl.create(:user, :admin)
+      expect(subject).to permit(admin, project)
+    end
+
+    it 'does not allow users assigned to other projects' do
+      other_project = FactoryGirl.create(:project)
+      assign_role!(user, :manager, other_project)
+      expect(subject).not_to permit(user, project)
+    end
   end
 end
