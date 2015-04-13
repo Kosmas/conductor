79c931798544f4ddf9843d382b58900b500c091e
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-13 13:48:25 +0200

    add assign_role viewer to user and update for current MEAP

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -1,30 +1,30 @@
 require 'spec_helper'
 
 feature 'Creating Tickets' do
+  let(:user) { FactoryGirl.create(:user) }
+
   before do
-    project = FactoryGirl.create(:project)
-    @user		= FactoryGirl.create(:user)
-    define_permission!(@user, 'view', project)
-    define_permission!(@user, 'create tickets', project)
-    sign_in_as!(@user)
+    project = FactoryGirl.create(:project, name: 'Mozilla Firefox')
+    assign_role!(user, :viewer, project)
+    login_as(user)
 
     visit '/'
-    click_link project.name
+    click_link 'Mozilla Firefox'
     click_link 'New Ticket'
   end
 
-  scenario 'Creating a ticket' do
+  scenario 'with valid attributes' do
     fill_in 'Title', with: 'Non-standards compliance'
     fill_in 'Description', with: 'My pages are ugly!'
     click_button 'Create Ticket'
 
     expect(page).to have_content('Ticket has been created.')
-    within '#ticket #author' do
-      expect(page).to have_content("Created by #{@user.email}")
+    within("#ticket #author") do
+      expect(page).to have_content("Created by #{user.email}")
     end
   end
 
-  scenario 'Creating a ticket without valid attributes fails' do
+  scenario 'with missing fields' do
     click_button 'Create Ticket'
 
     expect(page).to have_content('Ticket has not been created.')
@@ -32,31 +32,12 @@ feature 'Creating Tickets' do
     expect(page).to have_content("Description can't be blank")
   end
 
-  scenario 'Description must be longer than 10 characters' do
+  scenario 'with an invalid Description' do
     fill_in 'Title', with: 'Non-standards compliance'
     fill_in 'Description', with: 'it sucks'
     click_button 'Create Ticket'
 
     expect(page).to have_content('Ticket has not been created.')
     expect(page).to have_content('Description is too short')
-  end
-
-  scenario 'Creating a ticket with an attachment', js: true do
-    fill_in 'Title', with: 'Add documentation for blink tag'
-    fill_in 'Description', with: 'The blink tag has a speed attribute'
-
-    attach_file 'File #1', Rails.root.join('spec/fixtures/speed.txt')
-
-    click_link 'Add another file'
-    attach_file 'File #2', Rails.root.join('spec/fixtures/spin.txt')
-
-    click_button 'Create Ticket'
-
-    expect(page).to have_content('Ticket has been created.')
-
-    within('#ticket .assets') do
-      expect(page).to have_content('speed.txt')
-      expect(page).to have_content('spin.txt')
-    end
   end
 end
