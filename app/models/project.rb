3a62cc7af9b49075a2e0571c5658e659294d8c0b
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-13 13:41:24 +0200

    add has_many roles relation and update for current MEAP

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,13 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :tickets, dependent: :delete_all
-  has_many :permissions, as: :thing
+  has_many :roles, dependent: :delete_all
 
   validates :name, presence: true
-
-  scope :viewable_by, -> (user) do
-    joins(:permissions).where(permissions: { action: 'view', user_id: user.id })
-  end
-  scope :for, -> (user) do
-    user.admin? ? Project.all : Project.viewable_by(user)
-  end
 end
