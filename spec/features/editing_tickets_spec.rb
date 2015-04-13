8191bfaa8f3df5678b95a7ebe16ca6cf26c7fbdb
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-13 14:09:22 +0200

    add login for user and assign viewer role to user for project

--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -8,6 +8,9 @@ feature 'Editing tickets' do
   end
 
   before do
+    login_as(user)
+    assign_role!(user, :viewer, project)
+
     visit project_ticket_path(project, ticket)
     click_link 'Edit Ticket'
   end
