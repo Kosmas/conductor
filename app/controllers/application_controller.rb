2100db2652d65a7c339a87d43b1da47dbb8a0c60
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-14 16:47:11 +0200

    add rescue_from Pundit::NotAuthorizedError with not_authorized method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+
+  private
+
+  def not_authorized
+    redirect_to root_path, alert: "You aren't allowed to do that."
+  end
 end
