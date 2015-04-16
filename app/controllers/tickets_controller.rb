f7587fb565e4fe1f5d48736c73cb8a81b1ac7766
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-16 21:22:15 +0200

    update for current MEAP and add authorize method in show

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,30 +1,26 @@
 class TicketsController < ApplicationController
-  before_action :require_signin!
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
-  before_action :authorize_create!, only: [:new, :create]
-  before_action :authorize_update!, only: [:edit, :update]
-  before_action :authorize_delete!, only: :destroy
 
   def new
     @ticket = @project.tickets.build
-    @ticket.assets.build
   end
 
   def create
     @ticket = @project.tickets.build(ticket_params)
-    @ticket.user = current_user
+    @ticket.author = current_user
+
     if @ticket.save
-      flash[:notice] = "Ticket has been created."
+      flash[:notice] = 'Ticket has been created.'
       redirect_to [@project, @ticket]
     else
-      flash[:alert] = "Ticket has not been created."
-      render "new"
+      flash[:alert] = 'Ticket has not been created.'
+      render 'new'
     end
   end
 
   def show
-    @comment = @ticket.comments.build
+    authorize @ticket, :show?
   end
 
   def edit
@@ -36,9 +32,9 @@ class TicketsController < ApplicationController
 
       redirect_to [@project, @ticket]
     else
-      flash[:alert] = 'Ticket has not been updated.'
+      flash.now[:alert] = 'Ticket has not been updated.'
 
-      render action: 'edit'
+      render 'edit'
     end
   end
 
@@ -52,10 +48,7 @@ class TicketsController < ApplicationController
   private
 
   def set_project
-    @project = Project.for(current_user).find(params[:project_id])
-    rescue ActiveRecord::RecordNotFound
-    flash[:alert] = 'The project you were looking for could not be found.'
-    redirect_to root_path
+    @project = Project.find(params[:project_id])
   end
 
   def set_ticket
@@ -63,27 +56,6 @@ class TicketsController < ApplicationController
   end
 
   def ticket_params
-    params.require(:ticket).permit(:title, :description, assets_attributes: [:asset])
-  end
-
-  def authorize_create!
-    if !current_user.admin? && cannot?('create tickets'.to_sym, @project)
-      flash[:alert] = 'You cannot create tickets on this project.'
-      redirect_to @project
-    end
-  end
-
-  def authorize_update!
-    if !current_user.admin? && cannot?('edit tickets'.to_sym, @project)
-      flash[:alert] = 'You cannot edit tickets on this project.'
-      redirect_to @project
-    end
-  end
-
-  def authorize_delete!
-    if !current_user.admin? && cannot?('delete tickets'.to_sym, @project)
-      flash[:alert] = 'You cannot delete tickets from this project.'
-      redirect_to @project
-    end
+    params.require(:ticket).permit(:title, :description)
   end
 end
