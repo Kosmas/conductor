2ad28fc112a451426f81495c7caadc7fb083f115
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-17 17:53:40 +0200

    add specs for show permissions and removed pending tests

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -1,28 +1,49 @@
 require 'spec_helper'
 
 describe TicketPolicy do
-
-  let(:user) { User.new }
-
   subject { TicketPolicy }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  context 'permissions' do
+    subject { TicketPolicy.new(user, ticket) }
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    let(:user) { FactoryGirl.create(:user) }
+    let(:project) { FactoryGirl.create(:project) }
+    let(:ticket) { FactoryGirl.create(:ticket, project: project) }
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    context 'for anonymous users' do
+      let(:user) { nil }
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+      it { should_not permit_action :show }
+    end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context 'for viewers of the project' do
+      before { assign_role!(user, :viewer, project) }
+
+      it { should permit_action :show }
+    end
+
+    context 'for editors of the project' do
+      before { assign_role!(user, :editor, project) }
+
+      it { should permit_action :show }
+    end
+
+    context 'for managers of the project' do
+      before { assign_role!(user, :manager, project) }
+
+      it { should permit_action :show }
+    end
+
+    context 'for managers of other projects' do
+      before { assign_role!(user, :manager, FactoryGirl.create(:project)) }
+
+      it { should_not permit_action :show }
+    end
+
+    context 'for administrators' do
+      let(:user) { FactoryGirl.create(:user, :admin) }
+
+      it { should permit_action :show }
+    end
   end
 end
