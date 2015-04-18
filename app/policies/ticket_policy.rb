76cdccf15f168dbf93d579262336f2f29a7be70a
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-18 14:42:50 +0200

    use of new has_member? method

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -6,6 +6,6 @@ class TicketPolicy < ApplicationPolicy
   end
 
   def show?
-    user.try(:admin?) || record.project.roles.exists?(user_id: user)
+    user.try(:admin?) || record.project.has_member?(user)
   end
 end
