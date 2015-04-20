deac857dc350062a884e24e2b8b021626ff9be11
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-20 16:18:57 +0200

    add new update? method to override application default method

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -14,4 +14,10 @@ class TicketPolicy < ApplicationPolicy
         record.project.has_manager?(user) ||
         record.project.has_editor?(user)
   end
+
+  def update?
+    user.try(:admin?) ||
+        record.project.has_manager?(user) ||
+        (record.project.has_editor?(user) && record.author == user)
+  end
 end
