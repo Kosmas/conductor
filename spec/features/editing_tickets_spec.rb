6e878a0bb14d3afa4ed58c45e530d7b63bf43328
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-20 16:20:31 +0200

    changed role from viewer to manager to be able to edit tickets

--- spec/features/editing_tickets_spec.rb
+++ spec/features/editing_tickets_spec.rb
@@ -9,7 +9,7 @@ feature 'Editing tickets' do
 
   before do
     login_as(user)
-    assign_role!(user, :viewer, project)
+    assign_role!(user, :manager, project)
 
     visit project_ticket_path(project, ticket)
     click_link 'Edit Ticket'
