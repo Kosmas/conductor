6ba2f5825088fc1932b88dde55940c627e5a0409
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-21 16:09:06 +0200

    add scenarios for normal and admin users for the Delete Ticket link

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -38,6 +38,11 @@ feature 'hidden links' do
       visit project_ticket_path(project, ticket)
       expect(page).not_to have_link('Edit Ticket')
     end
+
+    scenario 'cannot see the Delete Ticket link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link('Delete Ticket')
+    end
   end
 
   context 'admin users' do
@@ -66,6 +71,11 @@ feature 'hidden links' do
     scenario 'can see the Edit Ticket link' do
       visit project_ticket_path(project, ticket)
       expect(page).to have_link('Edit Ticket')
+    end
+
+    scenario 'can see the Delete Ticket link' do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link('Delete Ticket')
     end
   end
 end
