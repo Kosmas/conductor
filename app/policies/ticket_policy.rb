69fff81a88daffb0ae84bd2efcd471591e4fb561
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-21 15:50:48 +0200

    override default destroy? permissions to allow admins and managers

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -20,4 +20,8 @@ class TicketPolicy < ApplicationPolicy
         record.project.has_manager?(user) ||
         (record.project.has_editor?(user) && record.author == user)
   end
+
+  def destroy?
+    user.try(:admin?) || record.project.has_manager?(user)
+  end
 end
