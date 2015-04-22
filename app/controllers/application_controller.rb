1e078fd962b6e76d6fd358f3f6b4585588bdfac8
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-22 10:50:22 +0200

    add after_action for verify_authorized and verify_policy_scoped for all controllers except devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
 
   include Pundit
 
+  after_action :verify_authorized, except: [:index], unless: :devise_controller?
+  after_action :verify_policy_scoped, only: [:index], unless: :devise_controller?
+
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
 
   private
