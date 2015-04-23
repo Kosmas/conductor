f67f411bd19cf414d21bd81e7925d623bc2fea74
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-23 19:10:15 +0200

    add has_many for roles and role_on method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :roles
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,5 +8,9 @@ class User < ActiveRecord::Base
 
   def to_s
     "#{email} (#{admin? ? 'Admin' : 'User'})"
+  end
+
+  def role_on(project)
+    roles.find_by(project_id: project).try(:name)
   end
 end
