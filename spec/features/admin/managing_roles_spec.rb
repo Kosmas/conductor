dabf650f8802d04f5baabb8754c057c044e41d28
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-24 14:30:34 +0200

    add specs for assigning roles to a new user

--- spec/features/admin/managing_roles_spec.rb
+++ spec/features/admin/managing_roles_spec.rb
@@ -29,5 +29,19 @@ feature "Admins can manage a user's roles" do
     expect(page).to have_content('Internet Explorer: Viewer')
     expect(page).to have_content('Sublime Text 3: Manager')
   end
+
+  scenario 'when assigning roles to a new user' do
+    click_link 'New User'
+
+    fill_in 'Email', with: 'newuser@conductor.com'
+    fill_in 'Password',with: 'password'
+
+    select 'Editor', from: 'Internet Explorer'
+    click_button 'Create User'
+
+    click_link 'newuser@conductor.com'
+    expect(page).to have_content('Internet Explorer: Editor')
+    expect(page).not_to have_content('Sublime Text 3')
+  end
 end
 
