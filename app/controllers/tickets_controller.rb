44a506e682f13ebd9832ca3e509d4dc8899059d4
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-25 13:21:04 +0200

    add asset_cache field in the permitted fields list

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -63,6 +63,6 @@ class TicketsController < ApplicationController
   end
 
   def ticket_params
-    params.require(:ticket).permit(:title, :description, :asset)
+    params.require(:ticket).permit(:title, :description, :asset, :asset_cache)
   end
 end
