e2aa7f7825e29e864229411505bfc53f1df4eb51
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-26 09:06:10 +0200

    add spec for multiple attachments

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -61,8 +61,27 @@ feature 'Creating Tickets' do
     fill_in 'Description', with: 'The blink tag has a speed attribute'
     click_button 'Create Ticket'
 
-    within("#ticket .asset") do
+    within('#ticket .asset') do
       expect(page).to have_content('speed.txt')
+    end
+  end
+
+  scenario 'with multiple attachments' do
+    fill_in 'Title', with: 'Add documentation for blink tag'
+    fill_in 'Description', with: 'The blink tag has a speed attribute'
+
+    attach_file 'File #1', Rails.root.join('spec/fixtures/speed.txt')
+    attach_file 'File #2', Rails.root.join('spec/fixtures/spin.txt')
+    attach_file 'File #3', Rails.root.join('spec/fixtures/gradient.txt')
+
+    click_button 'Create Ticket'
+
+    expect(page).to have_content('Ticket has been created.')
+
+    within('#ticket .assets') do
+      expect(page).to have_content('speed.txt')
+      expect(page).to have_content('spin.txt')
+      expect(page).to have_content('gradient.txt')
     end
   end
 end
