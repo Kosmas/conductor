108135590f030ff4e95b994e9f13f25bb0d00614
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-26 09:38:39 +0200

    regenarted asset model and and add mount_uploader

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,15 +1,5 @@
 class Asset < ActiveRecord::Base
-  mount_uploader :asset, AssetUploader
-
   belongs_to :ticket
 
-  before_save :update_content_type
-
-  private
-
-  def update_content_type
-    if asset.present? && asset_changed?
-      self.content_type = asset.file.content_type
-    end
-  end
+  mount_uploader :asset, AssetUploader
 end
