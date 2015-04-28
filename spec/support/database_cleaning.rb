5a0f0bfd82d9fff9b7c11ab456c9638802a7cfe2
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-04-28 15:34:11 +0200

    modify strategy from truncation to deletion

--- spec/support/database_cleaning.rb
+++ spec/support/database_cleaning.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :deletion
+    DatabaseCleaner.clean_with(:deletion)
   end
 
   config.before(:each) do
