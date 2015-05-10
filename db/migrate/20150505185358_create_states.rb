185624a0159bdc298f080b8132bcfcf47699187a
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-05-10 22:17:12 +0200

    create states model and update schema

---
+++ db/migrate/20150505185358_create_states.rb
@@ -0,0 +1,12 @@
+class CreateStates < ActiveRecord::Migration
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.string :color
+      t.string :background
+    end
+
+    add_column :tickets, :state_id, :integer, index: true
+    add_column :comments, :state_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502061548) do
+ActiveRecord::Schema.define(version: 20150505185358) do
 
   create_table "assets", force: :cascade do |t|
     t.string   "asset"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150502061548) do
     t.integer  "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "state_id"
   end
 
   add_index "comments", ["author_id"], name: "index_comments_on_author_id"
@@ -51,6 +52,12 @@ ActiveRecord::Schema.define(version: 20150502061548) do
   add_index "roles", ["project_id"], name: "index_roles_on_project_id"
   add_index "roles", ["user_id"], name: "index_roles_on_user_id"
 
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.string "background"
+  end
+
   create_table "tickets", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -58,6 +65,7 @@ ActiveRecord::Schema.define(version: 20150502061548) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "author_id"
+    t.integer  "state_id"
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
