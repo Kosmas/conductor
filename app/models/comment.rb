d5288beb40846d121b45e80d7eef191920370f8c
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-05-10 22:19:17 +0200

    add belongs_to state relation and set_ticket_state method in an after_create callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,18 @@
 class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :author, class_name: 'User'
+  belongs_to :state
+
+  delegate :project, to: :ticket
 
   validates :text, presence: true
 
-  delegate :project, to: :ticket
+  after_create :set_ticket_state
+
+  private
+
+    def set_ticket_state
+      ticket.state = state
+      ticket.save!
+    end
 end
