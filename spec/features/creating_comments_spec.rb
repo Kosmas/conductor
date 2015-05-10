9852b85755aa939d6141a8e7b0c94108f021e105
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-05-10 22:14:20 +0200

    add spec for changing a ticket's state

--- spec/features/creating_comments_spec.rb
+++ spec/features/creating_comments_spec.rb
@@ -8,6 +8,7 @@ feature 'Users can comment on tickets' do
   before do
     login_as(user)
     assign_role!(user, :manager, project)
+    FactoryGirl.create(:state, name: 'Open')
 
     visit '/'
     click_link project.name
@@ -29,5 +30,17 @@ feature 'Users can comment on tickets' do
     click_button 'Create Comment'
 
     expect(page).to have_content('Comment has not been created.')
+  end
+
+  scenario "when changing a ticket's state" do
+    click_link ticket.title
+    fill_in 'Text', with: 'This is a real issue'
+    select 'Open', from: 'State'
+    click_button 'Create Comment'
+
+    expect(page).to have_content('Comment has been created.')
+    within('#ticket .state') do
+      expect(page).to have_content('Open')
+    end
   end
 end
