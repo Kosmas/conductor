daa0a77ffc52d1fc174263ce2a8ca6fdbd623b9f
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-05-15 09:20:15 +0200

    add belongs_to previous_state and set_previous_state callback on before_create

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :author, class_name: 'User'
   belongs_to :state
+  belongs_to :previous_state, class_name: 'State'
 
   delegate :project, to: :ticket
 
   validates :text, presence: true
 
+  before_create :set_previous_state
   after_create :set_ticket_state
 
   private
@@ -14,5 +16,9 @@ class Comment < ActiveRecord::Base
     def set_ticket_state
       ticket.state = state
       ticket.save!
+    end
+
+    def set_previous_state
+      self.previous_state = ticket.state
     end
 end
