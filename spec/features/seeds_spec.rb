033a833967c54ebcbac78e0069b0c5a6b886aafa
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-06-11 15:14:27 +0200

    removed unused seeds spec

--- spec/features/seeds_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-feature 'Seed Data' do
-  scenario 'The basics' do
-    load Rails.root + 'db/seeds.rb'
-    user = User.where(email: 'admin@example.com').first!
-    project = Project.where(name: 'Conductor Beta').first!
-  end
-end
