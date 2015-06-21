2473ab40d3e4c95f9530677d54d2a1679c767594
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-06-21 14:55:57 +0200

    add tag_names parameter to allowed parameters list

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -65,7 +65,7 @@ class TicketsController < ApplicationController
   end
 
   def ticket_params
-    params.require(:ticket).permit(:title, :description,
+    params.require(:ticket).permit(:title, :description, :tag_names,
       assets_attributes: [:asset, :asset_cache])
   end
 end
